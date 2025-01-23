d4dffe8dc7c539b92a5676193751311bbc0cc194
Reshha <laleal.dev@gmail.com>
2025-01-23 15:29:07 +0800

    added hero page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header/Header.jsx"
+import Hero from "./components/Hero/Hero.jsx"
 import "./app.scss"
 
 export default function App() {
   return (
     <>
       <Header/>
+      <Hero/>
     </>
   )
 }
